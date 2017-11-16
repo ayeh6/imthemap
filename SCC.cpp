551361149ddacf4d525d6cc52aae23aebfb796d7
ayeh6 <ayeh6@ucmerced.edu>
2017-11-16 02:38:35 +0000

    7/10

--- SCC.cpp
+++ SCC.cpp
@@ -1,15 +1,22 @@
 #include <iostream>
 #include <stack>
 #include <list>
+#include <climits>
 using namespace std;
+
+int wow;
+int aye;
 
 class Graph
 {
 	int vert;
+	int lowest;
+	bool boolL;
 	list<int> *edge;
+	int output[];
 
 	void inputStack(int v, bool visited[], stack<int> &Stack);
-	void printOrder(int v, bool visited[]);
+	void printOrder(int v, bool visited[],bool boolL);
 
 public:
 	Graph(int vert);
@@ -21,20 +28,23 @@ public:
 Graph::Graph(int vert)
 {
 	this->vert = vert;
+	output[vert];
 	edge = new list<int>[vert];
 }
 
-void Graph::printOrder(int v, bool visited[])
+void Graph::printOrder(int v, bool visited[], bool boolL)
 {
 	visited[v] = true;
 	cout << v << " ";
-
+	wow++;
+	output[aye] = v;
+	aye++;
 	list<int>::iterator i;
 	for(i = edge[v].begin(); i != edge[v].end(); ++i)
 	{
 		if(!visited[*i])
 		{
-			printOrder(*i,visited);
+			printOrder(*i,visited,boolL);
 		}
 	}
 }
@@ -47,6 +57,7 @@ Graph Graph::getTranspose()
 		list<int>::iterator i;
 		for(i = edge[v].begin(); i != edge[v].end(); ++i)
 		{
+			//cout << "v is: " << v << " i is: " << *i << endl;
 			g.edge[*i].push_back(v);
 		}
 	}
@@ -75,8 +86,17 @@ void Graph::inputStack(int v, bool visited[], stack<int> &Stack)
 void Graph::printSCC()
 {
 	stack<int> Stack;
-
+	int num[vert];
+	int actual[vert];
+	for(int i = 0; i < vert; i++)
+	{
+		num[i] = 0;
+	}
+	wow = 0;
+	aye = 0;
+	int low = INT_MAX;
 	bool *visited = new bool[vert];
+	//cout << "vert is: " << vert << endl;
 	for(int i = 0; i < vert; i++)
 	{
 		visited[i] = false;
@@ -96,19 +116,61 @@ void Graph::printSCC()
 	{
 		visited[i] = false;
 	}
-
+	int k = 0;
 	while(Stack.empty() == false)
 	{
 		int v = Stack.top();
-		cout << "top of stack: " << v << endl;
+		boolL = false;
+		//cout << "top of stack: " << v << endl;
 		Stack.pop();
 
 		if(visited[v] == false)
 		{
-			G.printOrder(v,visited);
+			G.printOrder(v,visited,boolL);
+			num[k] = wow;
+			k++;
+			wow = 0;
 			cout << endl;
 		}
 	}
+	cout << "made it through while" << endl;
+	for(int i = 0; i < vert; i++)
+	{
+		//cout << num[i] << endl;
+	}
+	//cout << endl;
+	for(int i = 0; i < vert; i++)
+	{
+		//cout << G.output[i] << endl;
+	}
+	int a = 0;
+	int b = 0;
+	int c = 0;
+	while(num[a] != 0)
+	{
+		cout << "its here" << endl;
+		low = INT_MAX;
+		for(int i = 0; i < num[a]; i++)
+		{
+			if(low > G.output[b])
+			{
+				//cout << "low changes to " << G.output[b] << endl;
+				low = G.output[b];
+			}
+			b++;
+		}
+		for(int i = 0; i < num[a]; i++)
+		{
+			actual[G.output[c]] = low;
+			c++;
+		}
+		a++;
+	}
+	for(int i=0; i<vert; i++)
+	{
+		cout << actual[i] << endl;
+	}
+	//cout << endl;
 }
 
 int main()
